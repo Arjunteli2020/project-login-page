41c15b70e956d2639d9f24949083bfb6b643488c
Arjunteli2020 <arjunteli@2020gmail.com>
2026-06-10 12:21:31 +0530

    Added Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-dark text-white text-center p-3">
-     footer
+      © 2026 Login Page  ||  Create by Arjun
     </footer>
   );
 }
