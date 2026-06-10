16a4ef1f485acf2acd7cc354825c759797729bb0
Arjunteli2020 <arjunteli@2020gmail.com>
2026-06-10 12:17:40 +0530

    Added Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function Login() {
   return (
-   <h1>Login</h1>
+    <div className="container d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
+      <div className="card p-4" style={{ width: "300px" }}>
+        <h3 className="text-center mb-3">Login</h3>
+
+        <input type="email" className="form-control my-2" placeholder="Email" />
+        <input type="password" className="form-control my-2" placeholder="Password" />
+
+        <button className="btn btn-primary w-100 mt-2">Login</button>
+      </div>
+    </div>
   );
 }
 
